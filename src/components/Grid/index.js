19ace36f9430f197661d77d73e12ec26f7a905f8
Gabriela Florêncio <perflogabi@gmail.com>
2022-10-02 22:04:46 -0300

    semifinalizado

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -16,24 +16,22 @@ const Grid = ({ itens, setItens }) => {
         <
         C.Tr >
         <
-        C.Th width = { 40 } > Descrição < /C.Th> <
-        C.Th width = { 40 } > Valor < /C.Th> <
+        C.Th width = { 40 } > Descrição < /C.Th>   <
+        C.Th width = { 40 } > Valor < /C.Th>  <
         C.Th width = { 10 }
-        alignCenter >
-        Tipo <
-        /C.Th> <
-        C.Th width = { 10 } > < /C.Th> <
-        /C.Tr> <
-        /C.Thead> <
+        alignCenter > Tipo < /C.Th>   <
+        C.Th width = { 10 } > < /C.Th>   <
+        /C.Tr >  <
+        /C.Thead>   <
         C.Tbody > {
-            itens ? .map((item, index) => ( <
+            itens.map((index, item) => ( <
                 GridItem key = { index }
                 item = { item }
                 onDelete = { onDelete }
                 />
             ))
         } <
-        /C.Tbody> <
+        /C.Tbody>  <
         /C.Table>
     );
 };
