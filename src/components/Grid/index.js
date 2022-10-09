43a146e46858bdd17fb19dc044d56727e1dcde05
Gabriela Florêncio <perflogabi@gmail.com>
2022-10-08 23:55:17 -0300

    semiifinalizado

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,39 +1,34 @@
-import React from "react";
-import GridItem from "../GridItem";
-import * as C from "./styles";
+import React from 'react'
+import GridItem from '../GridItem'
+import * as C from './styles'
 
 const Grid = ({ itens, setItens }) => {
-    const onDelete = (ID) => {
-        const newArray = itens.filter((transaction) => transaction.id !== ID);
-        setItens(newArray);
-        localStorage.setItem("transactions", JSON.stringify(newArray));
-    };
+  const onDelete = (ID) => {
+    const newArray = itens.filter((transaction) => transaction.id !== ID)
+    setItens(newArray)
+    localStorage.setItem('transactions', JSON.stringify(newArray))
+  }
 
-    return ( <
-        C.Table >
-        <
-        C.Thead >
-        <
-        C.Tr >
-        <
-        C.Th width = { 40 } > Descrição < /C.Th>   <
-        C.Th width = { 40 } > Valor < /C.Th>  <
-        C.Th width = { 10 }
-        alignCenter > Tipo < /C.Th>   <
-        C.Th width = { 10 } > < /C.Th>   <
-        /C.Tr >  <
-        /C.Thead>   <
-        C.Tbody > {
-            itens.map((index, item) => ( <
-                GridItem key = { index }
-                item = { item }
-                onDelete = { onDelete }
-                />
-            ))
-        } <
-        /C.Tbody>  <
-        /C.Table>
-    );
-};
+  return (
+    <C.Table>
+      <C.Thead>
+        <C.Tr>
+          <C.Th width={40}> Descrição </C.Th> <C.Th width={40}> Valor </C.Th>{' '}
+          <C.Th width={10} alignCenter>
+            
+            Tipo
+          </C.Th>
+          <C.Th width={10}> </C.Th>
+        </C.Tr>
+      </C.Thead>
+      <C.Tbody>
+        
+        {itens.map((index, item) => (
+          <GridItem key={index} item={item} onDelete={onDelete} />
+        ))}
+      </C.Tbody>
+    </C.Table>
+  )
+}
 
-export default Grid;
+export default Grid
